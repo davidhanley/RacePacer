7ffb2c015557aedbc171447484f28a70b8156455
david hanley <davidhanley@davids-MacBook-Air.local>
2023-09-28 14:51:37 -0700

    cleaned up code

--- racepace.py
+++ racepace.py
@@ -12,40 +12,40 @@ def add_silence(audio, target_duration_ms):
         extended_audio = audio + silence
         return extended_audio
 
-def say_to_file( words, fn ):
-    v = os.popen( 'espeak "%s" -w %s' % (words,fn)).readlines()
+
+def say_to_file(words, fn):
+    _ = os.popen('espeak "%s" -w %s' % (words, fn)).readlines()
     return AudioSegment.from_wav(fn)
 
-def to_timestr(tis):
-    tis = int(tis)
-    m = tis / 60
-    s = tis % 60
+
+def to_timestr(time_in_seconds):
+    time_in_seconds = int(time_in_seconds)
+    minutes = time_in_seconds / 60
+    seconds = time_in_seconds % 60
     out = ""
-    if m>0:
-        out = "%d minutes, " %( m)
-    out = out + " %d seconds" % (s)
+    if minutes > 0:
+        out = "%d minutes, " % (minutes)
+    out = out + " %d seconds" % (seconds)
     return out
 
+
 def expand(floormap):
-    audio =  AudioSegment.from_wav( "/tmp/go.wav" )
+    audio = AudioSegment.from_wav("/tmp/go.wav")
     floor = 0
     time = 0.0
-    for (f,p) in floormap:
-        for _ in range(f):
+    for (floors, pace) in floormap:
+        for _ in range(floors):
             floor = floor + 1
-            time = time + p
-            audio = add_silence(audio , time * 1000)
-            text = "floor %d %s" % (floor ,to_timestr(time))
+            time = time + pace
+            audio = add_silence(audio, time * 1000)
+            text = "floor %d %s" % (floor, to_timestr(time))
             fn = "/tmp/%d.wav" % (floor)
             clip = say_to_file(text, fn)
-            print(clip)
             audio = audio + clip
     audio.export("pace.mp3", format="mp3")
+
 
 if __name__ == "__main__":
     with open('data.json', 'r') as file:
         data = json.load(file)
         expand(data)
-
-
-
